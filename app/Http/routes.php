300fffca05dfe5a1312d3910da4d11e7ba18ea65
Marcus Fulbright <fulbright.marcus@gmail.com>
2015-12-22 12:05:08 -0600

    initial set up to test dredd and phpunit with travis CI

---
+++ app/Http/routes.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Response;
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/message', function() {
+    return response()->json(['message'=>'Hello World!'])->withHeaders(['content-type' => 'application/json']);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
